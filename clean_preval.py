f27b71b9ca0cd9d517fd2b3b0c164662409c3aaa
Ulrich Tchuenkam <ulrichmobi2@gmail.com>
2020-09-01 04:29:38 +0200

    Add: Clean prevalence data from GEODES.
    This script automatically cleans the data downloaded from the project GEO DONNEES EN SANTE (GEODES).
    https://geodes.santepubliquefrance.fr/index.php#c=indicator&i=covid_dpps_epidemie.anxiete&s=12.%206-8%20juillet&t=a01&view=map1

---
+++ clean_preval.py
@@ -0,0 +1,25 @@
+# Script to clean the prevalence datasets
+
+from pathlib import Path, PurePath
+import pandas as pd
+import glob
+import os
+
+PREVAL_BASE = '/home/ulrich/Documents/master2/projects/projet_covid/data/prevalence/'
+PREVAL_FOLDERS = listdirs(PREVAL_BASE)
+
+def listdirs(path):
+    return [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+
+def clean_data():
+    for folder in PREVAL_FOLDERS:
+        columns = ['code', 'region',folder]
+        CLEAN_FOLDER = PREVAL_BASE+folder+"/clean/"
+        FILES = glob.glob(PREVAL_BASE+folder+"/*.csv")
+        for file in FILES:
+            df = pd.read_csv(file, encoding='utf-8', sep=";",header=2)
+            df.columns = columns
+            df.to_csv(CLEAN_FOLDER+PurePath(file).parts[-1], index=False)
+    print('done')
+
+
